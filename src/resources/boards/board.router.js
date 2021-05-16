69686dd1d1f37223eff032a918a5cc7a1fe34ff0
Volodymyr Sosevich <volodymyr_sosevich@epam.com>
2021-05-16 20:59:58 +0300

    Add try catch blocks

--- src/resources/boards/board.router.js
+++ src/resources/boards/board.router.js
@@ -4,49 +4,69 @@ const Board = require('./board.model');
 const boardsService = require('./board.service');
 
 router.route('/').get(async (req, res) => {
-    const boards = await boardsService.getAll();
-    res.json(boards.map(Board.toResponse));
+    try {
+        const boards = await boardsService.getAll();
+        return res.json(boards.map(Board.toResponse));
+    } catch (err) {
+        return res.status(404).send(err.message);
+    }
 });
 
 router.route('/:id').get(async (req, res) => {
-    const {id} = req.params;
-    const board = await boardsService.getById(id);
-    if (board) {
-        return res.json(Board.toResponse(board));
+    try {
+        const {id} = req.params;
+        const board = await boardsService.getById(id);
+        if (board) {
+            return res.json(Board.toResponse(board));
+        }
+        return res.status(404).send('404 Not found');
+    } catch (err) {
+        return res.status(404).send(err.message);
     }
-    return res.status(404).send('404 Not found');
 });
 
 router.route('/').post(async (req, res) => {
-    const {title, columns} = req.body
-    const boardObj = {
-        "id": uuid(),
-        "title": title,
-        "columns": columns
-    };
-    await boardsService.addBoard(boardObj);
-    res.status(201).json(Board.toResponse(boardObj));
+    try {
+        const {title, columns} = req.body
+        const boardObj = {
+            "id": uuid(),
+            "title": title,
+            "columns": columns
+        };
+        await boardsService.addBoard(boardObj);
+        return res.status(201).json(Board.toResponse(boardObj));
+    } catch (err) {
+        return res.status(404).send(err.message);
+    }
 });
 
 router.route('/:id').put(async (req, res) => {
-    const {id} = req.params;
-    const {title, columns} = req.body
-    const boardObj = {
-        "title": title,
-        "columns": columns
-    };
-    await boardsService.updateBoard(id, boardObj);
-    res.status(200).json(Board.toResponse(boardObj));
+    try {
+        const {id} = req.params;
+        const {title, columns} = req.body
+        const boardObj = {
+            "title": title,
+            "columns": columns
+        };
+        await boardsService.updateBoard(id, boardObj);
+        return res.status(200).json(Board.toResponse(boardObj));
+    } catch (err) {
+        return res.status(404).send(err.message);
+    }
 });
 
 router.route('/:id').delete(async (req, res) => {
-    const {id} = req.params;
-    const board = await boardsService.getById(id);
-    if (board) {
-        await boardsService.deleteBoard(board.id);
-        return res.status(204).json();
+    try {
+        const {id} = req.params;
+        const board = await boardsService.getById(id);
+        if (board) {
+            await boardsService.deleteBoard(board.id);
+            return res.status(204).json();
+        }
+        return res.status(404).send('Not found');
+    } catch (err) {
+        return res.status(404).send(err.message);
     }
-    return res.status(404).send('Not found');
 });
 
 module.exports = router;
--- src/resources/tasks/task.router.js
+++ src/resources/tasks/task.router.js
@@ -1,4 +1,4 @@
-const router = require('express').Router({mergeParams:true});
+const router = require('express').Router({mergeParams: true});
 const uuid = require('uuid').v4;
 const Task = require('./task.model');
 const tasksService = require('./task.service');
@@ -16,54 +16,70 @@ router.route('/').get(async (req, res) => {
 });
 
 router.route('/:id').get(async (req, res) => {
-    const {id} = req.params;
-    const task = await tasksService.getById(id);
-    if (task) {
-        return res.json(Task.toResponse(task));
+    try {
+        const {id} = req.params;
+        const task = await tasksService.getById(id);
+        if (task) {
+            return res.json(Task.toResponse(task));
+        }
+        return res.status(404).send('404 Not found');
+    } catch (err) {
+        return res.status(404).send(err.message);
     }
-    return res.status(404).send('404 Not found');
 });
 
 router.route('/').post(async (req, res) => {
-    const {boardIdParams} = req.params;
-    const {title, order, description, userId ,boardId, columnId} = req.body
-    const taskObj = {
-        "id": uuid(),
-        "title": title,
-        "order": order,
-        "description": description,
-        "userId": userId,
-        "boardId": boardId || boardIdParams,
-        "columnId": columnId
-    };
-    await tasksService.addTask(taskObj);
-    res.status(201).json(Task.toResponse(taskObj));
+    try {
+        const {boardIdParams} = req.params;
+        const {title, order, description, userId, boardId, columnId} = req.body
+        const taskObj = {
+            "id": uuid(),
+            "title": title,
+            "order": order,
+            "description": description,
+            "userId": userId,
+            "boardId": boardId || boardIdParams,
+            "columnId": columnId
+        };
+        await tasksService.addTask(taskObj);
+        return res.status(201).json(Task.toResponse(taskObj));
+    } catch (err) {
+        return res.status(404).send(err.message);
+    }
 });
 
 router.route('/:id').put(async (req, res) => {
-    const {id} = req.params;
-    const {title, order, description, userId ,boardId, columnId} = req.body
-    const taskObj = {
-        "id": uuid(),
-        "title": title,
-        "order": order,
-        "description": description,
-        "userId": userId,
-        "boardId": boardId,
-        "columnId": columnId
-    };
-    await tasksService.updateTask(id, taskObj);
-    res.status(200).json(Task.toResponse(taskObj));
+    try {
+        const {id} = req.params;
+        const {title, order, description, userId, boardId, columnId} = req.body
+        const taskObj = {
+            "id": uuid(),
+            "title": title,
+            "order": order,
+            "description": description,
+            "userId": userId,
+            "boardId": boardId,
+            "columnId": columnId
+        };
+        await tasksService.updateTask(id, taskObj);
+        return res.status(200).json(Task.toResponse(taskObj));
+    } catch (err) {
+        return res.status(404).send(err.message);
+    }
 });
 
 router.route('/:id').delete(async (req, res) => {
-    const {id} = req.params;
-    const task = await tasksService.getById(id);
-    if (task) {
-        await tasksService.deleteTask(task.id);
-        return res.status(204).json();
+    try {
+        const {id} = req.params;
+        const task = await tasksService.getById(id);
+        if (task) {
+            await tasksService.deleteTask(task.id);
+            return res.status(204).json();
+        }
+        return res.status(404).send('Not found');
+    } catch (err) {
+        return res.status(404).send(err.message);
     }
-    return res.status(404).send('Not found');
 });
 
 module.exports = router;
