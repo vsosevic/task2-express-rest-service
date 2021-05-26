78510a5e15d1777e919722ce01845926d3238c08
Volodymyr Sosevich <volodymyr_sosevich@epam.com>
2021-05-26 17:28:49 +0300

    Freactoring of the old code before task 4 implementation

--- src/resources/boards/board.model.js
+++ src/resources/boards/board.model.js
@@ -1,14 +1,18 @@
 const uuid = require('uuid').v4;
+const Column = require('./board.column.model');
 
 class Board {
   constructor({
     id = uuid(),
-    title = 'Board title',
-    columns = []
+    title,
+    columns
   } = {}) {
     this.id = id;
     this.title = title;
-    this.columns = columns;
+    this.columns = columns.map(
+        (item) =>
+            new Column({ id: item.id, title: item.title, order: item.order })
+    );
   }
 
   static toResponse(board) {
