3a5fb75cb8dd0676b9c2eeff834d36b0d2dd7e99
Volodymyr Sosevich <volodymyr_sosevich@epam.com>
2021-06-05 18:28:43 +0300

    fix - correct next() placement

--- src/resources/boards/board.router.ts
+++ src/resources/boards/board.router.ts
@@ -1,24 +1,26 @@
 import {ReasonPhrases, StatusCodes} from 'http-status-codes';
-import express, {Request, Response} from 'express';
+import express, {Request, Response, NextFunction } from 'express';
 import { Board } from "./board.model";
 import * as boardsService from './board.service'
 
 const router = express.Router();
 
-router.route('/').get(async (_req: Request, res: Response) => {
+router.route('/').get(async (_req: Request, res: Response, next: NextFunction) => {
     try {
         const boards = await boardsService.getAll();
+        next();
         return res.json(boards.map(Board.toResponse));
     } catch (err) {
         return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(err.message);
     }
 });
 
-router.route('/:id').get(async (req: Request, res: Response) => {
+router.route('/:id').get(async (req: Request, res: Response, next: NextFunction) => {
     try {
         const {id} = req.params;
         const board = await boardsService.getById(String(id));
         if (board) {
+            next();
             return res.status(StatusCodes.OK).json(Board.toResponse(board));
         }
         return res.status(StatusCodes.NOT_FOUND).send(ReasonPhrases.NOT_FOUND);
@@ -27,20 +29,22 @@ router.route('/:id').get(async (req: Request, res: Response) => {
     }
 });
 
-router.route('/').post(async (req: Request, res: Response) => {
+router.route('/').post(async (req: Request, res: Response, next: NextFunction) => {
     try {
         const board = await boardsService.addBoard(req.body);
+        next();
         return res.status(StatusCodes.CREATED).json(Board.toResponse(board));
     } catch (err) {
         return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(err.message);
     }
 });
 
-router.route('/:id').put(async (req: Request, res: Response) => {
+router.route('/:id').put(async (req: Request, res: Response, next: NextFunction) => {
     try {
         const {id} = req.params;
         const board = await boardsService.updateBoard(String(id), req.body);
         if (board) {
+            next();
             return res.status(200).json(Board.toResponse(board));
         }
         return res.status(StatusCodes.NOT_FOUND).send(ReasonPhrases.NOT_FOUND);
@@ -49,12 +53,13 @@ router.route('/:id').put(async (req: Request, res: Response) => {
     }
 });
 
-router.route('/:id').delete(async (req: Request, res: Response) => {
+router.route('/:id').delete(async (req: Request, res: Response, next: NextFunction) => {
     try {
         const {id} = req.params;
         const board = await boardsService.getById(String(id));
         if (board) {
             await boardsService.deleteBoard(board.id);
+            next();
             return res.status(StatusCodes.NO_CONTENT).json();
         }
         return res.status(StatusCodes.NOT_FOUND).send(ReasonPhrases.NOT_FOUND);
--- src/resources/tasks/task.router.ts
+++ src/resources/tasks/task.router.ts
@@ -9,8 +9,8 @@ router.route('/').get(async (_req: Request, res: Response, next: NextFunction) =
     try {
         const tasks = await tasksService.getAll();
         if (tasks) {
-            return res.status(StatusCodes.OK).json(tasks.map(Task.toResponse));
             next();
+            return res.status(StatusCodes.OK).json(tasks.map(Task.toResponse));
         }
         return res.status(StatusCodes.NOT_FOUND).send(ReasonPhrases.NOT_FOUND);
     } catch (err) {
@@ -23,8 +23,8 @@ router.route('/:id').get(async (req: Request, res: Response, next: NextFunction)
         const {id} = req.params;
         const task = await tasksService.getById(String(id));
         if (task) {
-            return res.json(Task.toResponse(task));
             next();
+            return res.json(Task.toResponse(task));
         }
         return res.status(StatusCodes.NOT_FOUND).send(ReasonPhrases.NOT_FOUND);
     } catch (err) {
@@ -39,8 +39,8 @@ router.route('/').post(async (req: Request, res: Response, next: NextFunction) =
         const {title, order, description, userId, columnId} = req.body
         const taskObj = new Task({title, order, description, boardId, columnId, userId});
         const task = await tasksService.addTask(taskObj);
-        return res.status(StatusCodes.CREATED).json(Task.toResponse(task));
         next();
+        return res.status(StatusCodes.CREATED).json(Task.toResponse(task));
     } catch (err) {
         return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(err.message);
     }
@@ -51,9 +51,9 @@ router.route('/:id').put(async (req: Request, res: Response, next: NextFunction)
         const {id} = req.params;
         const task = await tasksService.updateTask(String(id), req.body);
         if (task) {
+            next();
             return res.status(StatusCodes.OK).json(Task.toResponse(task));
         }
-        next();
         return res.status(StatusCodes.NOT_FOUND).send(ReasonPhrases.NOT_FOUND);
     } catch (err) {
         return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(err.message);
