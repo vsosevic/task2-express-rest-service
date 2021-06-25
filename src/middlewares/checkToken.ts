36fba5a4cfee50a003fa26b4b21b6d2a375d6a73
Volodymyr Sosevich <volodymyr_sosevich@epam.com>
2021-06-26 00:21:25 +0300

    fix - lint errors

--- src/middlewares/checkToken.ts
+++ src/middlewares/checkToken.ts
@@ -7,19 +7,23 @@ function checkToken(req: Request, res: Response, next: NextFunction) {
     switch (req.url) {
         case '/':
         case '/login':
-        case '/doc':
+        case '/doc': {
             return next();
-    }
-    const authHeader = req.headers.authorization;
-    if (authHeader !== undefined) {
-        const [type, token] = authHeader.split(' ');
-        if (type !== 'Bearer' || !token) {
-            return res.status(StatusCodes.UNAUTHORIZED).send('Wrong auth scheme');
         }
-        jwt.verify(token, String(JWT_SECRET_KEY));
-        return next();
+        default: {
+            const authHeader = req.headers.authorization;
+            if (authHeader !== undefined) {
+                const [type, token] = authHeader.split(' ');
+                if (type !== 'Bearer' || !token) {
+                    return res.status(StatusCodes.UNAUTHORIZED).send('Wrong auth scheme');
+                }
+                jwt.verify(token, String(JWT_SECRET_KEY));
+                return next();
+            }
+            return res.status(StatusCodes.UNAUTHORIZED).send(ReasonPhrases.UNAUTHORIZED);
+        }
     }
-    return res.status(StatusCodes.UNAUTHORIZED).send(ReasonPhrases.UNAUTHORIZED);
+
 }
 
 export { checkToken }
